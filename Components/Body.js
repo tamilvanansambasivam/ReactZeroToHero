c19d97bacaa471ec03da35a243d7bd6c692323f4
tamilvanan <tamilvanansambasivam@gmail.com>
2023-12-03 15:17:19 +0530

    Update code

--- Components/Body.js
+++ Components/Body.js
@@ -2,13 +2,57 @@ import React from "react";
 import Card from "./Card";
 
 export default function Body() {
+  const dataList = [
+    {
+      id: 1,
+      imageSrc:
+        "https://images.unsplash.com/photo-1512621776951-a57141f2eefd?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "Spaghetti Bolognese",
+      cuisine: "Italian",
+      starRatings: 4.5,
+      deliveryTime: "30 minutes",
+    },
+    {
+      id: 2,
+      imageSrc:
+        "https://images.unsplash.com/photo-1512621776951-a57141f2eefd?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "Sushi Platter",
+      cuisine: "Japanese",
+      starRatings: 4.8,
+      deliveryTime: "40 minutes",
+    },
+    {
+      id: 3,
+      imageSrc:
+        "https://images.unsplash.com/photo-1512621776951-a57141f2eefd?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "Vegetarian Pizza",
+      cuisine: "Italian",
+      starRatings: 4.0,
+      deliveryTime: "35 minutes",
+    },
+    {
+      id: 4,
+      imageSrc:
+        "https://images.unsplash.com/photo-1512621776951-a57141f2eefd?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "Chicken Tikka Masala",
+      cuisine: "Indian",
+      starRatings: 4.2,
+      deliveryTime: "50 minutes",
+    },
+    {
+      id: 5,
+      imageSrc:
+        "https://images.unsplash.com/photo-1512621776951-a57141f2eefd?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "Green Salad",
+      cuisine: "Healthy",
+      starRatings: 4.7,
+      deliveryTime: "25 minutes",
+    },
+  ];
+
   return (
     <div>
-      {/* 
-        search bar
-        card container
-    */}
-
+      {/* search */}
       <div className="flex">
         <input
           className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lgblock w-full p-2 m-2 "
@@ -16,11 +60,11 @@ export default function Body() {
         />
         <button>search</button>
       </div>
-
+      {/* card container */}
       <div className="flex flex-wrap bg-red-400">
-        {Array.from({ length: 10 }).map((_, index) => (
-          <Card key={index} />
-        ))}
+        {dataList.map((data) => {
+          return <Card data={data} key={data.id} />;
+        })}
       </div>
     </div>
   );
--- Components/Card.js
+++ Components/Card.js
@@ -1,6 +1,7 @@
 import React from "react";
 
-export default function Card() {
+export default function Card(props) {
+  const { name, cuisine, starRatings, deliveryTime } = props.data;
   return (
     <div className=" p-4">
       <div className="h-64 w-44 bg-white rounded-lg overflow-hidden p-2">
@@ -8,10 +9,10 @@ export default function Card() {
           src="https://images.unsplash.com/photo-1512621776951-a57141f2eefd?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           className="rounded-lg shadow-lg"
         />
-        <p className="text-lg font-bold">Food Name</p>
-        <p>cuisines</p>
-        <p> star ratings</p>
-        <p> delivery time</p>{" "}
+        <p className="text-lg font-bold">{name}</p>
+        <p>{cuisine}</p>
+        <p> {starRatings}</p>
+        <p> {deliveryTime}</p>
       </div>
     </div>
   );
